768904d6315e3ba422e26bd6ed78652703260770
Krystal-Liang <krystl3@uw.edu>
2018-03-07 16:20:40 -0800

    unfinished data wrangling

--- krystalquestion.R
+++ krystalquestion.R
@@ -2,8 +2,9 @@ library("dplyr")
 library("ggplot2")
 library(maps)
 library(tidyr)
-
-
+library(fiftystater)
+library(plotly)
+library(mapproj)
 
 # Reads the csv file that contains all the data needed for Q3
 shooting.data <- read.csv(file="police_killings.csv", stringsAsFactors = FALSE)
@@ -17,13 +18,37 @@ race.prop <- shooting.data %>%
   group_by(raceethnicity) %>%
   summarize(n = n()) 
 
-# Plot that shows the proportion of races killed 
-race.prop.bar.graph <- ggplot(race.prop, aes(raceethnicity, n)) + 
-  geom_bar(stat = "identity") +
-  xlab("Race/Ethnicity") + ylab("Number of People Killed") +
-  ggtitle("Proportion Of People Killed Based On Race/Ethnicity") +
-  theme_bw() +
-  theme(plot.title = element_text(hjust = 0.5))
+#Using plotly to make the bar graph that shows the proportion of races killed
+bar.graph.x <- list(title = "Race/Ethnicity",
+                    titlefont = f,
+                    tickangle = 10,
+                    margin = 10,
+                    categoryorder = "array",
+                    categoryarray = c("Native American", "Asian/Pacific Islander", "Unknown", 
+                                      "Hispanic/Latino", "Black", "White"))
+bar.graph.y <- c(4, 10, 15, 67, 135, 236)
+bar.graph.text <- c('4 People Killed', '10 People Killed', '15 People Killed', 
+                    '67 People Killed', '135 People Killed', '236 People Killed')
+bar.graph.data <- data.frame(bar.graph.x, bar.graph.y, bar.graph.text)
+
+f <- list(
+  family = "Arial",
+  size = 18,
+  color = "black"
+)
+
+bar.graph <- plot_ly(bar.graph.data, x = ~bar.graph.x, y = ~bar.graph.y, type = 'bar', 
+             text = bar.graph.text,
+             marker = list(color = 'rgb(158,202,225)',
+                           line = list(color = 'rgb(8,48,107)', width = 1.5))) %>%
+            layout(title = "Proportion Of People Killed Based On Race/Ethnicity",
+                   titlefont = f,
+                   margin = list(b = 100,
+                                 t = 50),
+                   xaxis = bar.graph.x,
+                   yaxis = list(title = "Number of People Killed",
+                                tickangle = 0,
+                                titlefont = f))
 
 ## In America the percentage of people who are White are about 70% leaving the rest of the
 ## people colored of "unidentified". If you add up the values for those who are of color vs.
@@ -33,6 +58,13 @@ race.prop.bar.graph <- ggplot(race.prop, aes(raceethnicity, n)) +
 ## white people are higher so less white people died altogether. This is true to what the 
 ## media displays as well, as we see in the media that usually more people of color are killed
 ## compared to white people. 
+
+## According to the United States Census Bureu data, in 2017, the estimated population for the 
+## country was 325,719,178 people. More specifically, according to 2016 data, 61.3% of the 
+## population were only white with no Hispanic or Latino origin. In contrast, 1.3% were 
+## American Indian and Alaskan Native alone, 5.7% were Asian alone, 0.2% were Hawaiian and/or
+## Pacific Islander, 13.3% were Black or African American.
+
 
 ## You may think at first, "but the bar shows that more white people were killed altogether,
 ## but that's when you have to read into the US and how our population's races are proportioned
@@ -48,14 +80,28 @@ race.pov.with.means <- race.pov.with.state %>%
   summarize(mean = round(mean(pov, na.rm = TRUE)), n = n())
 
 race.pov.data <- left_join(race.pov.with.means, states.data)
+race.pov.data$State <- tolower(race.pov.data$State)
+colnames(race.pov.data)[4]<-("state")
 
-colnames(race.pov.data)[4]<-("region")
-race.pov.data$region <- tolower(race.pov.data$region)
+crimes <- data.frame(state = tolower(rownames(USArrests)), USArrests)
 
-race.pov.with.map.data <- left_join(usa.map, race.pov.data) 
+race.pov.data.states <- left_join(crimes, race.pov.data) %>%
+  select(state, mean, n)
 
-race.poverty.map <- ggplot() + geom_polygon(data = race.pov.with.map.data, 
-                                            color = "white", 
-                                            fill = "black",
-                                            aes(x = long, y = lat, 
-                                                group = group))
+
+state.names <- race.pov.data.states[1]
+
+# map_id creates the aesthetic mapping to the state name column in your data
+pov.and.death.map <- ggplot(race.pov.data.states, aes(map_id = state)) + 
+  # map points to the fifty_states shape data
+  geom_map(aes(fill = mean), color = "white", map = fifty_states) + 
+  expand_limits(x = fifty_states$long, y = fifty_states$lat) +
+  coord_map() +
+  scale_x_continuous(breaks = NULL) + 
+  scale_y_continuous(breaks = NULL) +
+  labs(x = "", y = "") +
+  ggtitle("Average Poverty Rates vs. Number of Deaths") +
+  theme(legend.position = "bottom", 
+        panel.background = element_blank())
+# add border boxes to AK/HI
+pov.and.death.map + fifty_states_inset_boxes()
