4a60f5a24515ebc0f1a2a8d814e1458724e43d57
Krystal-Liang <krystl3@uw.edu>
2018-03-07 23:54:56 -0800

    krystal's shiny app

--- krystalapp.R
+++ krystalapp.R
@@ -3,28 +3,23 @@ source("krystalquestion.R")
 
 # Define UI for viz app ----
 my.ui <- fluidPage(
-  plotOutput("plot1", click = "plot_click"),
-  verbatimTextOutput("info"),
-  
-  # Input: Selecter for the state that you would like to view for ----
-  selectInput("State", label="Select A State", 
-              c("State", state.names), multiple = FALSE)
-)
+    
+    # Output: Tabset w/ plot, summary, and table ----
+    tabsetPanel(type = "tabs",
+                tabPanel("Bar Graph", plotOutput("plot")),
+                tabPanel("Map", plotOutput("map"))
+    )
+  )
 
 # Define the server for the viz app
 my.server <- function(input, output) {
   
-  viz.react <- reactive( {
-    return(pov.and.death.map)
+  output$plot <- renderPlot({
+    return(bar.graph)
   })
   
-  output$plot1 <- renderPlot({
-    viz.react()
-  })
-  
-  output$info <- renderText({
-    paste("Race/Ethnicity = ", input$plot_click$x, "\nNumber of People Killed = "
-          , input$plot_click$y)
+  output$map <- renderPlot({
+    return(p)
   })
 }
 
